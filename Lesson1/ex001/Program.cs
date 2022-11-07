bba31070ab5914d8dcf7de51c7097c7430a85e77
vyky1983 <veronica.cernenchii@mail.ru>
2022-11-07 21:16:09 +0300

    ADD new inf

--- Lesson1/ex001/Program.cs
+++ Lesson1/ex001/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+Clear();
+
+int n = 5;
+int[] array = new int[n];
+for (int i = 0; i < n; i++)
+    array[i] = Convert.ToInt32(ReadLine());
+WriteLine("[" + string.Join(", ", array) + "]");
+
+//[4, 5, 3, 1, 2]
+//O(n)
+//[1, 2, 3, 4, 5] -- O(n * log n) 
+// ((5 + 1)/2) * 5 -- O(1)
