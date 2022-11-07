93085a64555f273b46fed74610abe2cfe67fb923
vyky1983 <veronica.cernenchii@mail.ru>
2022-11-07 21:25:58 +0300

    new inf ex001

--- Lesson1/ex001/Program.cs
+++ Lesson1/ex001/Program.cs
@@ -10,5 +10,11 @@ WriteLine("[" + string.Join(", ", array) + "]");
 
 //[4, 5, 3, 1, 2]
 //O(n)
-//[1, 2, 3, 4, 5] -- O(n * log n) 
-// ((5 + 1)/2) * 5 -- O(1)
+//[1, 2, 3, 4, 5] -- O(n * log n) Быстрая сортировка
+// ((5 + 1)/2) * 5 -- O(1) СУММА АРИФМЕТИЧЕСКИХ ПРОГРЕСИИ
+//n < n * log(n) + 1
+
+int summa = 0;
+for (int i = 0; i < n; i++)
+    summa += array[i];
+System.Console.WriteLine(summa); //O(n)
