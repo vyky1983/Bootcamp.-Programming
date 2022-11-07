a1cc94ecd4e5a412ecfbfe5540af94914010e1d6
vyky1983 <veronica.cernenchii@mail.ru>
2022-11-08 00:11:51 +0300

    new INF

--- Lesson1/ex002/Program.cs
+++ Lesson1/ex002/Program.cs
@@ -1,15 +1,26 @@
 ﻿using static System.Console;
+
 Clear();
 
 //O(n²)
 
 int n = Convert.ToInt32(ReadLine());
-for (int i = 1; i <= n; i++)
+int[,] matrix = new int[n, n];
+for (int i = 0; i < n; i++)
 {
-    for (int j = 1; j <= n; j++) 
+    for (int j = i; j < n; j++)
     {
-     Console.Write(i * j);
-     Console.Write("\t");
-     }
-     System.Console.WriteLine();
+        matrix[i, j] = (i + 1) * (j + 1);
+        matrix[j, i] = (i + 1) * (j + 1);
+    }
+    WriteLine();
+}
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Write(matrix[i, j]);
+        Write(" ");
+    }
+    System.Console.WriteLine();
 }
