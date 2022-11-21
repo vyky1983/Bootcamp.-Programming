e12932d04719d5590e511019042e2ea1f881b2c0
vyky1983 <veronica.cernenchii@mail.ru>
2022-11-21 19:25:38 +0300

    modified

--- Less16/bin.py
+++ Less16/bin.py
@@ -1,2 +1,9 @@
 import math 
 math.log(10)
+
+x=(1, 2, 5)
+print(type(x))
+
+y=[5, 3, 'vyky']
+print(type(y))
+
--- Lesson13/main.py
+++ Lesson13/main.py
@@ -1,10 +1,15 @@
 import cv2
 
-img = cv2.imread('D:/GB/bootcamp/ExploringOpenCV/test.jpg')
+img = cv2.imread('test.jpg')
 
-img = cv2.resize(img, (500, 500))
+img = cv2.size(img, (500, 500))
 
 cv2.imshow('Result', img)
 
 cv2.waitKey(0)
 print(img)
+
+images_per_class = 80
+fixed_size       = tuple((500, 500))
+train_path       = "dataset/train"
+
